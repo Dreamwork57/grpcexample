22b7338dd2cc7801008e7615e5a4fb50d2026f28
Lucas <lucas.bellissimo@gmail.com>
2022-02-19 16:27:56 +0100

    Init repo

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # grpcexample
-Demo project for gRPC implementation with Spring Boot
---
+++ src/test/java/com/lbel/grpcexample/GrpcexampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lbel.grpcexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrpcexampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
